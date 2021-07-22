f715d3f18d80259abf4154817ed93e78385d9553
Mattaru <gctdljgkjnm1@gmail.com>
2021-07-22 18:31:33 +0300

    settings

---
+++ my_cos/my_cos/local_settings.py
@@ -0,0 +1,39 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+ADMIN_NAME = 'Admin'
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        # 'TEST': {
+        #     'NAME': 'test_' + os.getenv('DB_NAME'),
+        # },
+    }
+}
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFIELDS_DIRS = [STATIC_DIR]
+
+# Django debug toolbar
+# INTERNAL_IPS = [
+#     '127.0.0.1',
+# ]
